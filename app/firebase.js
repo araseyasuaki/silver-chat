72c1a9e2d7418c8c3eb3efe2432d755e5a1f29b4
Your Name <your.email@example.com>
2024-07-24 14:20:59 +0900

    firebase初期化

---
+++ app/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAdLxdXJ4WM5g-JW7gWQ6kZ3EpRbjpTCXI",
+  authDomain: "chat-app-55c59.firebaseapp.com",
+  projectId: "chat-app-55c59",
+  storageBucket: "chat-app-55c59.appspot.com",
+  messagingSenderId: "256547840638",
+  appId: "1:256547840638:web:24ec9e424805e9af20ee2f"
+};
+
+const app = initializeApp(firebaseConfig);
