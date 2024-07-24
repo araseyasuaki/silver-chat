496e96c0fc95d0021aa2c112f0c4526e96188620
Your Name <your.email@example.com>
2024-07-24 14:41:49 +0900

    indexファイルloginへのlink追加

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { StyleSheet, View, Text, Image } from 'react-native';
+import { Link } from 'expo-router'
 
 const ticketImg1 = require('../assets/images/ticket-img-1.png');
 const ticketImg2 = require('../assets/images/ticket-img-2.png');
 
 const index = () => {
   return (
-    <View style={s.main}>
+    <Link href='login' asChild>
+      <View style={s.main}>
       <Image source={ticketImg1}/>
       <Image source={ticketImg2}/>
       <Text>Rrrrrrip</Text>
-    </View>
+      </View>
+    </Link>
   )
 }
 
