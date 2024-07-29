1b0e899f56cf16aa430291e83f1e35ee539d0d7e
Your Name <your.email@example.com>
2024-07-29 09:18:30 +0900

    firebase更新

--- app/firebase.js
+++ app/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAdLxdXJ4WM5g-JW7gWQ6kZ3EpRbjpTCXI",
-  authDomain: "chat-app-55c59.firebaseapp.com",
-  projectId: "chat-app-55c59",
-  storageBucket: "chat-app-55c59.appspot.com",
-  messagingSenderId: "256547840638",
-  appId: "1:256547840638:web:24ec9e424805e9af20ee2f"
+  apiKey: "AIzaSyBMpmTk0LiQ7R6dlkD9TAWtYB9WtDGajIo",
+  authDomain: "chat-app-ff7e9.firebaseapp.com",
+  projectId: "chat-app-ff7e9",
+  storageBucket: "chat-app-ff7e9.appspot.com",
+  messagingSenderId: "102410004892",
+  appId: "1:102410004892:web:71c85d9dab6e5c8a5e0059"
 };
 
 const app = initializeApp(firebaseConfig);
