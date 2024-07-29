62b350f4a396569f01fb2f8cc8a57f4119f9970a
Your Name <your.email@example.com>
2024-07-29 09:24:06 +0900

    chatのlayout追加

---
+++ app/home/(tabs)/chat/_layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Stack } from 'expo-router'
+
+const layout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name='index'/>
+    </Stack>
+  )
+}
+
+export default layout
